1a0e1a016f53760039d35f865075cd6540ab06e1
Oleksii Morenets <oleksii.morenets@gmail.com>
2019-06-23 17:59:25 +0300

    Lesson 5 DI (Dependency Injection) constructor-arg ref

--- src/main/java/ua/training/TestSpring.java
+++ src/main/java/ua/training/TestSpring.java
@@ -7,8 +7,7 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music musicBean = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
